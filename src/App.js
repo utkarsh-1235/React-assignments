770a1bc043d577d5fa69cc48468309c029579453
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-08-12 13:46:35 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Person from './Components/person';
 import Button from './Components/Button';
 import Header from './Components/Header';
 import List from './Components/List';
+import { useState } from 'react';
 
 function App(props) {
-  const ButtonClick = ()=>{
-    console.log("button clicked");
+  const [x,setX] = useState(0);
+ 
+  const onClick=()=>{
+    console.log(x)
+     console.log(setX(x+1));
   }
   return (
-   
     <div>
       <Person name="utkarsh saxena" age={21}/>
-      <Button text="clickMe" onClick={ButtonClick}/>
+      <Button text="clickMe" onClick={onClick}/>
       <Header header="Namaste Utkarsh" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt natus quasi sint. Optio recusandae temporibus magni distinctio magnam minus soluta!"/>
       <List items="google"/>
 
---
+++ src/Components/person.js
@@ -0,0 +1,14 @@
+
+const person=({name,age})=>{
+   
+    return(
+        <>
+          <p>
+            Name:{name}, Age:{age}
+            </p>
+          
+        </>
+    )
+}
+
+export default person;
